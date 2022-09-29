8aee748ca3efe02ceed5d4d6c0205c90959d8ec4
Samuel Cardoso <samuelparacontato@gmail.com>
2022-09-29 20:47:57 -0300

    fix: renaming test

--- src/test/kotlin/br/puc/springhandson/MovieServiceTest.kt
+++ src/test/kotlin/br/puc/springhandson/MovieServiceTest.kt
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringHandsonApplicationTests {
+class MovieServiceTest {
 	@Autowired
 	private lateinit var movieService: MovieService
 
